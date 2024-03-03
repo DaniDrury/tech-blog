c3502d2808e3322746767ff5e6be611c521ea410
DaniDrury <145943483+DaniDrury@users.noreply.github.com>
2024-03-02 18:36:12 -0800

    Comment model first draft complete

---
+++ models/Comment.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {};
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: User,
+        key: id,
+      }
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    modelName: 'comment'
+  }
+);
+
+module.exports = Comment;
