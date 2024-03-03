c065d545120b2e22cc65ee477a1cda80c3f67a39
DaniDrury <145943483+DaniDrury@users.noreply.github.com>
2024-03-02 18:26:52 -0800

    Post model, first draft, created

---
+++ models/Post.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+// const Comment = require('./Comment');
+
+class Post extends Model {};
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: User,
+        key: id,
+      }
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    modelName: 'post',
+  }
+);
+
+module.exports = Post;
