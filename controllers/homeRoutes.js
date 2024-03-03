c936d388746a5443e53101281f9c04b99ecd195e
DaniDrury <145943483+DaniDrury@users.noreply.github.com>
2024-03-03 13:48:46 -0800

    created homepage.hbs and added homepage route to controller

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,20 +1,28 @@
 const router = require('express').Router();
-const {} = require('../models');
+const { User, Post } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-// sequelize get expression - findAll blogPosts
+// sequelize get expression - findAll blog posts
+  const postData = await Post.findAll({
+    include: [
+      {
+        model: User,
+        attributes: ['name'],
+      },
+    ],
+  });
 
-// serialize data so the template can read it?
+  if (!postData) {
+    throw new Error('No posts found');
+  }
 
+// serialize data so the template can read it
+  const posts = postData.map((post) => post.get({ plain: true }));
+
+  console.log(posts);
 // pass data and session flag into tempate
-  res.render('homepage', {
-    // replace variable
-    serialized_data,
-    logged_in: req.session.logged_in
-  })
-
-
-  // error handling somewhere in here, not necessarily below
+  res.render('homepage', { posts, logged_in: req.session.logged_in });
 });
 
+module.exports = router;
