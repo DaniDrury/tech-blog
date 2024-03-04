fab45cdf453f95a8517f1b629f790c59f177ec65
DaniDrury <145943483+DaniDrury@users.noreply.github.com>
2024-03-03 21:36:55 -0800

    fixed signup bug, separated signup functionality into signup.js, signup working

--- public/js/login.js
+++ public/js/login.js
@@ -22,42 +22,4 @@ const loginFormHandler = async (event) => {
   }
 };
 
-const signupFormHandler = async (event) => {
-  event.preventDefault();
-
-  const name = document.querySelector('#username-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
-
-  console.log('\n -------------------- \n');
-  console.log(name);
-  console.log(email);
-  console.log(password);
-  console.log('\n -------------------- \n');
-
-  if (name && email && password) {
-    const response = await fetch('/api/users/signup', {
-      method: 'POST',
-      body: JSON.stringify({ name, email, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    console.log('\n -------------------- \n');
-    console.log(response);
-    console.log('\n -------------------- \n');
-
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert(response.statusText);
-    }
-  }
-};
-
-document
-  .querySelector('#login-form')
-  .addEventListener('submit', loginFormHandler);
-
-document
-  .querySelector('#signup-form')
-  .addEventListener('submit', signupFormHandler);
+document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
