572fc6b230896662f56c9784838a606c3f5a9578
DaniDrury <145943483+DaniDrury@users.noreply.github.com>
2024-03-03 22:00:41 -0800

    updated signup to user username and password without requesting email

--- public/js/signup.js
+++ public/js/signup.js
@@ -2,25 +2,14 @@ const signupFormHandler = async (event) => {
   event.preventDefault();
 
   const name = document.querySelector('#username-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
 
-  console.log('\n -------------------- \n');
-  console.log(name);
-  console.log(email);
-  console.log(password);
-  console.log('\n -------------------- \n');
-
-  if (name && email && password) {
+  if (name && password) {
     const response = await fetch('/api/users/signup', {
       method: 'POST',
-      body: JSON.stringify({ name, email, password }),
+      body: JSON.stringify({ name, password }),
       headers: { 'Content-Type': 'application/json' },
     });
-
-    console.log('\n -------------------- \n');
-    console.log(response);
-    console.log('\n -------------------- \n');
 
     if (response.ok) {
       document.location.replace('/');
