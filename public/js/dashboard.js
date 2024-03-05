5a5aa10a263bcb6a5b47f39bbf2015e51b9b1fa6
DaniDrury <145943483+DaniDrury@users.noreply.github.com>
2024-03-04 16:43:28 -0800

    beginning to add Edit Post functionality from dashboard page

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -28,5 +28,23 @@ const newPostFormHandler = async (event) => {
   };
 };
 
+const postClickHandler = (event) => {
+  //  Need this to send user to /post/edit where the post-form-edit page is rendered
+  //  need to send post data into form
+};
+
+const editPostFormHandler = (event) => {
+  // send fetch PUT request to update post
+};
+
+const deletePostHandler = (event) => {
+  //  from the post-form-edit page, delete the post
+};
+
+// use ?. chain operator to check if element exists before trying to addEventListiner
+// if element does not exist, action is skipped
+document.querySelectorAll('.post-card')?.forEach(e => e.addEventListener("click", postClickHandler));
 document.querySelector('#newPostBtn')?.addEventListener("click", newPostBtnHandler);
 document.querySelector('#new-post-form')?.addEventListener("submit", newPostFormHandler);
+document.querySelector('#deleteBtn')?.addEventListener('click', deletePostHandler);
+document.querySelector('#edit-post-form')?.addEventListener('submit', editPostFormHandler);
