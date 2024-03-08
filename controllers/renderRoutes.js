3f71e124c89de34fc1897dc1f7ffc3d8fef84a59
DaniDrury <145943483+DaniDrury@users.noreply.github.com>
2024-03-07 16:58:01 -0800

    code review prior to submission

--- controllers/renderRoutes.js
+++ controllers/renderRoutes.js
@@ -3,7 +3,7 @@ const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-// sequelize get expression - findAll blog posts
+// sequelize get expression - findAll blog posts with username data and ordered by creation date
   const postData = await Post.findAll({
     include: [
       {
@@ -16,17 +16,19 @@ router.get('/', async (req, res) => {
     ],
   });
 
-// serialize data so the template can read it
+// serialize data so the template can read it -- these methods are repeated in following render functions
   const posts = postData.map((post) => post.get({ plain: true }));
 
 // pass data and session flag into tempate
   res.render('homepage', { posts, logged_in: req.session.logged_in });
 });
 
+// renders "New Post" page with input form, checks for logged-in authorization using withAuth middleware
 router.get('/post/new', withAuth, (req, res) => {
-  res.render('post-form-new');
+  res.render('post-form-new', { logged_in: req.session.logged_in });
 });
 
+// render individual post page - does not require logged in status to view
 router.get('/post/:id', async (req, res) => {
   const id = req.params.id;
 
@@ -55,6 +57,7 @@ router.get('/post/:id', async (req, res) => {
   res.render('post', { post, logged_in: req.session.logged_in });
 });
 
+// render user dashboard, requires logged-in status
 router.get('/dashboard', withAuth, async (req, res) => {
   // sequelize get expression - findAll blog posts where user_id matches logged_in user_id
   const postData = await Post.findAll({
@@ -77,6 +80,7 @@ router.get('/dashboard', withAuth, async (req, res) => {
   res.render('dashboard', { posts, logged_in: req.session.logged_in });
 });
 
+// render post-form-edit view for a specific post with that post data passed to the .hbs
 router.get('/dashboard/:id', withAuth, async (req, res) => {
   const id = req.params.id;
   
