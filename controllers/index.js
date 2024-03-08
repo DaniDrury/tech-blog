f2fd43a92f6b55d64effee04a7cc278335e27dcd
DaniDrury <145943483+DaniDrury@users.noreply.github.com>
2024-03-07 16:07:31 -0800

    updated after renaming of homeRoutes to renderRoutes

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
+const renderRoutes = require('./renderRoutes');
 
-router.use('/', homeRoutes);
+router.use('/', renderRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
